f1939cbfd5090a36822d7dc94c40e5d3f82da927
Shreekant Kalwar <92009663+shreekant17@users.noreply.github.com>
2024-01-12 19:36:05 +0530

    Delete Scripts directory

--- Scripts/UploadPost/delete_comment.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../LoginRegister/check_login.php');
-$sql="delete from comments where CommentId='$commentId'";
-$conn->query($sql);
-$sql="update allPost set CommentsCount=CommentsCount-1 where postId='$postId'";
-$conn->query($sql);
-header("location:../../index.php");
-?>
